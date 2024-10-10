578e001e02273e85227f76f966ac8cdb122c9fc0
Jason Skillman <jason_skillman@outlook.com>
2024-10-10 17:35:24 -0400

    Refactor to use 128 bit guid instead of string

---
+++ Editor/TagEditor.cs
@@ -0,0 +1,21 @@
+﻿namespace JasonSkillman.ScriptableTags.Editor {
+	using UnityEditor;
+	using UnityEngine;
+
+	[CustomEditor(typeof(Tag))]
+	public class TagEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+
+			Tag tag = (Tag)target;
+
+			EditorGUILayout.LabelField("GUID", tag.GuidString);
+        
+			if(GUILayout.Button("Generate new GUID")) {
+				tag.CreateNewGuid();
+			}
+		}
+	}
+}
